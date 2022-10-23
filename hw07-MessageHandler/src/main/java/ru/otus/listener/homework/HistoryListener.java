03007e01ea67ab693e3ace487878799bb0ce0630
Igor Scheglov <scheglov.igor@gmail.com>
2022-10-23 22:43:33 +0300

    hw07-fix2

--- hw07-MessageHandler/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-MessageHandler/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -8,16 +8,15 @@ import java.util.*;
 // 1. я правильно понимаю, что тут нужно было менять сам класс, а не наследоваться от него?
 public class HistoryListener implements Listener, HistoryReader {
 
-    private final Map<Long, Deque<Message>> messageDequeMap = new HashMap<>();
+    private final Map<Long, Message> messageMap = new HashMap<>();
 
     @Override
     public void onUpdated(Message msg) {
-        Deque<Message> messageDeque = messageDequeMap.computeIfAbsent(msg.getId(), l -> new ArrayDeque<>());
-        messageDeque.push(msg.copy());
+        Message messageDeque = messageMap.put(msg.getId(), msg.copy());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        return Optional.ofNullable(messageDequeMap.get(id).poll());
+        return Optional.ofNullable(messageMap.get(id));
     }
 }
